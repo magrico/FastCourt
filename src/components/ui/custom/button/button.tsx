1c374990c830704f9ae39cd0bc3a8940040a4e80
magrico <magro440@gmail.com>
2025-01-31 11:20:32 +0000

    Initial commit with project files

---
+++ src/components/ui/custom/button/button.tsx
@@ -0,0 +1,15 @@
+import '@/components/ui/custom/button/button.css'
+
+const Button = () =>{
+    return(
+        <>
+        <button className="button">
+          <div className="dots_border"></div>
+          <span className="text_button">Button</span>
+        </button>
+        </>
+    )
+}
+
+export default Button;
+
---
+++ src/pages/Nonstops.tsx
@@ -0,0 +1,9 @@
+function Nonstops(){
+    return(
+    <div>
+        This is Nonstop page
+        </div>
+    )
+}
+
+export default Nonstops
